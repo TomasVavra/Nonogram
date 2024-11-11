92ceca73c52f83dbfd7b6a8e916b6e5661bcc330
TomasVavra <tomvavra1@gmail.com>
2024-11-12 00:26:39 +0100

    commit 8

--- nonogram.py
+++ nonogram.py
@@ -4,52 +4,71 @@ def find_number_of_rows():
     with open("instruction.txt","r") as file:
         for line in file:
             if "rows" in line:
-                rows = int(re.search(r'\d+', line)[0])
-    return rows
+                result = int(re.search(r'\d+', line)[0])
+    return result
 
 def find_number_of_columns():
     with open("instruction.txt", "r") as file:
         for line in file:
             if "columns" in line:
-                columns = int(re.search(r'\d+', line)[0])
-    return columns
+                result = int(re.search(r'\d+', line)[0])
+    return result
 
-def find_raw_instruction_rows(number_of_rows):
+def find_raw_instruction_rows(l_number_of_rows):
     with open("instruction.txt", "r") as file:
-        rows_instruction = []
+        result = []
         counter = 0
         start_counter = False
         for line in file:
             if "rows" in line:
                 start_counter = True
             if start_counter:
-                if 0 < counter <= number_of_rows:
-                    rows_instruction.append(line.strip())
+                if 0 < counter <= l_number_of_rows:
+                    result.append(line.strip())
                 counter += 1
-    return rows_instruction
+    return result
 
-def find_raw_instruction_columns(number_of_columns):
+def find_raw_instruction_columns(l_number_of_columns):
     with open("instruction.txt", "r") as file:
-        columns_instruction = []
+        result = []
         counter = 0
         start_counter = False
         for line in file:
             if "columns" in line:
                 start_counter = True
             if start_counter:
-                if 0 < counter <= number_of_columns:
-                    columns_instruction.append(line.strip())
+                if 0 < counter <= l_number_of_columns:
+                    result.append(line.strip())
                 counter += 1
-    return columns_instruction
+    return result
 
-def convert_raw_instruction_to_2d_array(raw_instruction):
-    array_2d = []
-    for line in raw_instruction:
+def convert_raw_instruction_to_2d_array(l_raw_instruction):
+    result = []
+    for line in l_raw_instruction:
         #print(type(line))
-        array_2d.append(re.findall(r'\d+', line))
+        result.append(re.findall(r'\d+', line))
         #print(type(re.findall(r'\d+', line)))
-    return array_2d
+    return result
 
+def make_sum_row(l_number_of_rows,l_rows_instruction):
+    result = [0 for row in range(l_number_of_rows)]
+    for index, row in enumerate(l_rows_instruction):
+        numbers = 0
+        for item in row:
+            result [index] += int(item)
+            numbers += 1                 #at least 1 space between ech numbers
+        result[index] += numbers - 1     #numbers -1 = spaces
+    return result
+
+def make_sum_column(l_number_of_columns,l_columns_instruction):
+    result = [0 for column in range(l_number_of_columns)]
+    for index, column in enumerate(l_columns_instruction):
+        numbers = 0
+        for item in column:
+            result [index] += int(item)
+            numbers += 1                 #at least 1 space between ech numbers
+        result[index] += numbers - 1     #numbers -1 = spaces
+    return result
 
 number_of_rows = find_number_of_rows()
 number_of_columns = find_number_of_columns()
@@ -57,27 +76,20 @@ rows_raw_instruction = find_raw_instruction_rows(number_of_rows)
 columns_raw_instruction = find_raw_instruction_columns(number_of_rows)
 rows_instruction = convert_raw_instruction_to_2d_array(rows_raw_instruction)
 columns_instruction = convert_raw_instruction_to_2d_array(columns_raw_instruction)
-'''
-print(number_of_rows)
-print(number_of_columns)
-[print(x) for x in rows_instruction]
-print()
-[print(x) for x in columns_instruction]
-'''
-
-sum_row = [0 for x in range(number_of_rows)]
-for idx, row in enumerate(rows_instruction):
-    for item in row:
-        sum_row [idx] += int(item)
-
+sum_row = make_sum_row(number_of_rows,rows_instruction)
+sum_column = make_sum_column(number_of_columns,columns_instruction)
 print(sum_row)
-
+print(sum_column)
+print()
 
 matrix = [[0 for row in range(number_of_rows)] for column in range(number_of_columns)]
 
-for row in range(number_of_rows):
-    for column in range(number_of_columns):
-        matrix[row][column] = "#"
+for index, row in enumerate(rows_instruction):
+    for item in row:
+        if int(item) > number_of_rows - sum_row[int(index)]:
+            print(index,sum_row[int(index)] , item)
+
+
 
 #for row in matrix:
 #   print(row)

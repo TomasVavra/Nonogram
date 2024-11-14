9a5044ab06141cc0f34d975bc0e2fb0aba1ab079
TomasVavra <tomvavra1@gmail.com>
2024-11-14 18:47:22 +0100

    commit 17

--- nonogram.py
+++ nonogram.py
@@ -13,6 +13,12 @@ def find_dimensions() -> List[int]:
                 result  [1]= int(re.search(r'\d+', line)[0])
     return result
 
+def get_row(l_matrix, row_index):
+    return l_matrix[row_index]
+
+def get_col(l_matrix, col_index):
+    return [row[col_index] for row in l_matrix]
+
 def find_raw_instruction_rows(l_number_of_rows: int):
     with open(input_file, "r") as file:
         result = []
@@ -49,7 +55,6 @@ def convert_raw_instruction_to_2d_array(l_raw_instruction: List[str]) -> List[Li
     for i, row in enumerate(result):                       #convert them to int
         for j, item in enumerate(row):
             result[i][j] = int(item)
-            print(result[i][j], type(result[i][j]))
     return result
 
 def find_row_instruction():
@@ -62,17 +67,6 @@ def find_col_instruction():
     l_dimension = find_dimensions()
     cols_raw_instruction = find_raw_instruction_cols(l_dimension[1])
     result = convert_raw_instruction_to_2d_array(cols_raw_instruction)
-    return result
-
-#sum of the numbers in each row or column + number of spaces
-def make_sum_row_or_col(l_rows_or_cols_instruction: List[List[int]]) -> List[int]:
-    #result = [0 for row in range(l_number_of_rows_or_cols)]
-    result = []
-    for row_or_col in l_rows_or_cols_instruction:
-        sum = 0
-        for item in row_or_col:
-            sum += int(item) +1
-        result.append(sum-1)     #numbers -1 = spaces
     return result
 
 # in nice way with col and row indexes
@@ -98,28 +92,21 @@ def print_matrix(l_matrix):
         print("|")
 
 # line can be row or column
-def paint_overlap_in_line(l_matrix_line: List[int], l_instruction_line: List[int], l_sum_instruction_line: int):
+def paint_overlap_in_line(l_matrix_line: List[int], l_instruction_line: List[int]):
     offset = 0
     for item_in_instruction in l_instruction_line:
-        overlap = int(item_in_instruction) + sum(l_instruction_line) + len(l_instruction_line) - 1 - len(l_matrix_line)
+        overlap = int(item_in_instruction) + sum(l_instruction_line) + len(l_instruction_line) - 1 - len(l_matrix_line)     #spaces = numbers in instruction(len()) -1
         if overlap > 0:
             for i in range(offset + int(item_in_instruction) - overlap, offset + int(item_in_instruction)):  # paint overlap in row
                 l_matrix_line[i] = "#"
         offset += int(item_in_instruction) + 1
 
 # number_of_cols for rows_instruction to iterate through items in 1 row
-def paint_overlap (l_matrix: List[List[int]], l_number_of_cols_or_rows: int, l_rows_or_cols_instruction: List[List[int]], sum_rows_or_cols: List[int], is_row: bool) -> None:
+def paint_overlap (l_matrix: List[List[int]], l_rows_or_cols_instruction: List[List[int]], is_row: bool) -> None:
     for row_or_col_index, row_or_col in enumerate(l_rows_or_cols_instruction):
-        offset = 0
-        for item_in_instruction in row_or_col:
-            overlap = int(item_in_instruction) + sum_rows_or_cols[int(row_or_col_index)] - l_number_of_cols_or_rows
-            if overlap > 0:
-                for i in range(offset + int(item_in_instruction) - overlap, offset + int(item_in_instruction)):     #paint overlap in row
-                    if is_row:
-                        l_matrix[row_or_col_index][i] = "#"
-                    else:
-                        l_matrix[i][row_or_col_index] = "#"
-            offset += int(item_in_instruction) + 1
+        l_matrix_line = get_row(l_matrix, row_or_col_index) if is_row else get_col(l_matrix, row_or_col_index)
+        paint_overlap_in_line(l_matrix_line, l_rows_or_cols_instruction[row_or_col_index])
+
 
 def count_packs_of_hashes_in_line(l_matrix_row_or_col: List[int]) -> int:
     result = 0
@@ -159,24 +146,22 @@ dimension = find_dimensions()
 number_of_rows = dimension[0]
 number_of_cols = dimension[1]
 print(number_of_rows,number_of_cols)
-
+print()
 rows_instruction = find_row_instruction()
 cols_instruction = find_col_instruction()
 
-sum_row = make_sum_row_or_col(rows_instruction)
-sum_col = make_sum_row_or_col(cols_instruction)
-print(sum_row)
-print(sum_col)
-print()
+# sum_row = make_sum_row_or_col(rows_instruction)
+# sum_col = make_sum_row_or_col(cols_instruction)
+# print(sum_row)
+# print(sum_col)
+
 matrix = [[col for col in range(number_of_cols)] for row in range(number_of_rows)]
 
-#paint_overlap_in_line(matrix[7], rows_instruction[7], sum_row[7])
-print(matrix[7])
-print(type(rows_instruction[0][0]))
+# paint_overlap_in_line(matrix[7], rows_instruction[7])
+# print(matrix[7])
 
-print(number_of_cols, sum_row[7])
 
-#paint_overlap(matrix, number_of_cols, rows_instruction, sum_row, is_row = True)     # number_of_cols to iterate through item in 1 row
+paint_overlap(matrix, rows_instruction, is_row = True)
 
 print()
 print(matrix[7])

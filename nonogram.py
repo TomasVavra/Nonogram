4e903dd096f109fce3bdecd6b8f93dc224e3eabb
TomasVavra <tomvavra1@gmail.com>
2024-11-14 14:39:25 +0100

    commit 14

--- nonogram.py
+++ nonogram.py
@@ -52,7 +52,7 @@ def convert_raw_instruction_to_2d_array(l_raw_instruction: List[str]) -> List[Li
         result.append(re.findall(r'\d+', line))
     return result
 
-#sum of numbers in each row or column + number of spaces
+#sum of the numbers in each row or column + number of spaces
 def make_sum_row_or_col(l_number_of_rows_or_cols: int, l_rows_or_cols_instruction: List[List[int]]) -> List[int]:
     result = [0 for row in range(l_number_of_rows_or_cols)]
     for index, row in enumerate(l_rows_or_cols_instruction):
@@ -63,40 +63,77 @@ def make_sum_row_or_col(l_number_of_rows_or_cols: int, l_rows_or_cols_instructio
         result[index] += numbers - 1     #numbers -1 = spaces
     return result
 
+def print_matrix(l_matrix):     # in nice way with col and row indexes
+    print("      ", end="")
+    for idx, item in enumerate(l_matrix[0]):
+        print(idx, " ", end="")
+        if idx < 10:
+            print(" ", end="")
+    print()
+    print("      ", end="")
+    [print("----", end="") for item in l_matrix[0]]
+    print()
+    for idx, row in enumerate(l_matrix):
+        print(idx, " ",end="")
+        if idx < 10:
+            print(" ",end="")
+        print("| ",end="")
+        for item in row:
+            print(item," ",end="")
+            if type(item) != int or item < 10:
+                print(" ",end="")
+        print("|")
+
+def paint_overlap_in_line(l_matrix_row_or_col: List[int]):
+    pass
+
 # number_of_cols for rows_instruction to iterate through items in 1 row
-def paint_overlap (l_matrix: List[List[int]], l_number_of_rows_or_cols: int, l_rows_or_cols_instruction: List[List[int]], sum_row_or_col: List[int], is_row: bool) -> None:
+def paint_overlap (l_matrix: List[List[int]], l_number_of_cols_or_rows: int, l_rows_or_cols_instruction: List[List[int]], sum_row_or_col: List[int], is_row: bool) -> None:
     for row_or_col_index, row_or_col in enumerate(l_rows_or_cols_instruction):
         offset = 0
         for item_in_instruction in row_or_col:
-            overlap = int(item_in_instruction) + sum_row_or_col[int(row_or_col_index)] - l_number_of_rows_or_cols
+            overlap = int(item_in_instruction) + sum_row_or_col[int(row_or_col_index)] - l_number_of_cols_or_rows
             if overlap > 0:
-                counter = 0                 #is finished
                 for i in range(offset + int(item_in_instruction) - overlap, offset + int(item_in_instruction)):     #paint overlap in row
                     if is_row:
                         l_matrix[row_or_col_index][i] = "#"
-                        counter += 1
                     else:
                         l_matrix[i][row_or_col_index] = "#"
-                        counter += 1
-                if counter == int(item_in_instruction) and i+1 < l_number_of_rows_or_cols:  #is finished
-                    l_matrix[row_or_col_index][i + 1] = "."                                 #is finished
             offset += int(item_in_instruction) + 1
 
-#předělat, pokud je pocet skupinek = počet čisel v zadani, můžu vyškrtávat
-def is_finished (l_matrix: List[List[int]], l_number_of_rows_or_cols: int, l_rows_or_cols_instruction: List[List[int]], is_row: bool) -> None:
+def count_packs_of_hashes_in_line(l_matrix_row_or_col: List[int]) -> int:
+    result = 0
+    hash_counter = 0
+    for item in l_matrix_row_or_col:
+        hash_counter = hash_counter + 1 if item == "#" else 0
+        if hash_counter == 1:
+            result += 1
+    return result
+
+
+#předělat, pokud pocet skupinek = počet čisel v zadani, můžu vyškrtávat
+def is_finished (l_matrix: List[List[int]], l_number_of_cols_or_rows: int, l_rows_or_cols_instruction: List[List[int]], is_row: bool) -> None:
     for row_or_col_index, row_or_col in enumerate(l_rows_or_cols_instruction):
         for item_in_instruction in row_or_col:
-            counter = 0
-            for i in range(l_number_of_rows_or_cols):
+            hash_counter = 0
+            for i in range(l_number_of_cols_or_rows):
                 if is_row:
-                    counter = counter + 1 if l_matrix[row_or_col_index][i] == "#" else 0
-                    if counter == int(item_in_instruction) and i+1 < l_number_of_rows_or_cols:
-                        l_matrix[row_or_col_index][i+1] = "."
-                        print("x")
-                else:
-                    counter +=1 if l_matrix[i][row_or_col_index] == "#" else 0
-                    if counter == item_in_instruction and i+1 < l_number_of_rows_or_cols:
-                        l_matrix[i+1][row_or_col_index] = "."
+                    hash_counter = hash_counter + 1 if l_matrix[row_or_col_index][i] == "#" else 0
+                    if hash_counter == 1:
+                        pass
+
+
+            if pack_counter == len(row_or_col):
+                for i in range(l_number_of_cols_or_rows):
+                    if is_row:
+                        hash_counter = hash_counter + 1 if l_matrix[row_or_col_index][i] == "#" else 0
+                        if hash_counter == int(item_in_instruction) and i+1 < l_number_of_cols_or_rows:
+                            l_matrix[row_or_col_index][i+1] = "."
+                            # print("x")
+                # else:
+                #     hash_counter +=1 if l_matrix[i][row_or_col_index] == "#" else 0
+                #     pass# if hash_counter == item_in_instruction and i+1 < l_number_of_cols_or_rows:
+                #     #     l_matrix[i+1][row_or_col_index] = "."
 
 
 
@@ -114,35 +151,19 @@ print()
 
 matrix = [[col for col in range(number_of_cols)] for row in range(number_of_rows)]
 paint_overlap(matrix, number_of_cols, rows_instruction, sum_row, is_row = True)     # number_of_cols to iterate through item in 1 row
-#is_finished(matrix, number_of_rows, rows_instruction, is_row = True)
 
+print(matrix[7])
+packs = count_packs_of_hashes_in_line(matrix[7])
+print(packs)
 
-paint_overlap(matrix, number_of_rows, cols_instruction, sum_col, is_row = False)
+column = [row[5] for row in matrix]
+print(column)
+packs = count_packs_of_hashes_in_line(column)
+print(packs)
 
+#is_finished(matrix, number_of_cols, rows_instruction, is_row = True)
 
+#paint_overlap(matrix, number_of_rows, cols_instruction, sum_col, is_row = False)
+print()
+print_matrix(matrix)
 
-
-
-
-for idx, row in enumerate(matrix):
-    print(idx, " ",end="")
-    if idx < 10:
-        print(" ",end="")
-    print("| ",end="")
-    for item in row:
-        print(item," ",end="")
-        if type(item) != int or item < 10:
-            print(" ",end="")
-    print("|")
-
-# for idx, row in enumerate(matrix):
-#     print(idx,end="")
-#     for item in row:
-#         if type(item) == int:
-#             print(" ",item," ",end="")
-#         else:
-#             print(item,end="")
-
-#     print(idx, row)
-# print()
-# print(matrix[2][0])
